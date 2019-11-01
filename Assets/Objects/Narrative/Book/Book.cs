2db004a5827dfc04d0a7640e1656b5db0d9cbee3
Moe-Baker <moe4baker@gmail.com>
2019-11-01 16:40:53 +0200

    Books and Pages

---
+++ Assets/Objects/Narrative/Book/Book.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+namespace Game.Narrative
+{
+    [RequireComponent(typeof(Animator))]
+	public class Book : MonoBehaviour
+	{
+		public IList<Page> Pages { get; protected set; }
+        int index;
+
+        public UIProperty UI;
+        [Serializable]
+        public class UIProperty
+        {
+            public Button next;
+        }
+
+        public Animator Animator { get; protected set; }
+
+        private void Awake()
+        {
+            Animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            Pages = GetComponentsInChildren<Page>();
+
+            foreach (var page in Pages)
+            {
+                page.Init(this);
+            }
+
+            UI.next.onClick.AddListener(Next);
+        }
+
+        public void Open()
+        {
+            Animator.SetTrigger("Open");
+        }
+
+        public void Begin()
+        {
+            Begin(0);
+        }
+
+        public void Begin(Page page)
+        {
+            index = Pages.IndexOf(page);
+
+            Begin(index);
+        }
+        void Begin(int index)
+        {
+            UI.next.interactable = false;
+
+            Pages[index].OnEnd += PageEndCallback;
+
+            Pages[index].Begin();
+        }
+
+        private void PageEndCallback()
+        {
+            UI.next.interactable = true;
+        }
+
+        public void Next()
+        {
+            index++;
+
+            if(index >= Pages.Count)
+            {
+                End();
+            }
+            else
+            {
+                Begin(index);
+            }
+        }
+
+        public event Action OnEnd;
+        void End()
+        {
+            OnEnd?.Invoke();
+        }
+    }
+}
---
+++ Assets/Objects/Narrative/Book/Page/Page.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Narrative
+{
+	public class Page : MonoBehaviour
+	{
+        Book book;
+        int index;
+        public void Init(Book book)
+        {
+            this.book = book;
+
+            index = book.Pages.IndexOf(this);
+        }
+
+		public void Begin()
+        {
+            book.Animator.Play("Page " + (index + 1));
+
+            End();
+        }
+
+        public event Action OnEnd;
+        protected void End()
+        {
+            OnEnd?.Invoke();
+        }
+	}
+}
